768ae56880af49a60aea19d4f16c306eef74faff
rafuaero <rafuaero@gmail.com>
2025-02-13 13:19:00 -0300

    updates to script

--- pdf_unlock.py
+++ pdf_unlock.py
@@ -1,18 +1,15 @@
 from PyPDF2 import PdfReader, PdfWriter
+import sys
 
-# Caminho para o PDF bloqueado
-arquivo_bloqueado = '2025-01-15_Fatura_Rafael Ferreira Costacurta_287569_BTG.pdf'
+locked_file = input("Enter the name and path of the locked pdf") 
+password = input("Enter the password to unlock the pdf"
+unlocked_file = input("enter the name of the unlocked file") 
 
-# Caminho para salvar o PDF desbloqueado
-arquivo_desbloqueado = "fatura_jan25.pdf"
-
-# Senha do PDF
-senha = '68999801187'
 
 # Ler o PDF protegido
-reader = PdfReader(arquivo_bloqueado)
+reader = PdfReader(locked_file)
 if reader.is_encrypted:
-    reader.decrypt(senha)
+    reader.decrypt(password)
 
 # Criar um novo PDF sem senha
 writer = PdfWriter()
@@ -20,7 +17,7 @@ for page in reader.pages:
     writer.add_page(page)
 
 # Salvar o novo PDF sem senha
-with open(arquivo_desbloqueado, 'wb') as output_pdf:
+with open(unlocked_file, 'wb') as output_pdf:
     writer.write(output_pdf)
 
-print(f"O arquivo {arquivo_desbloqueado} foi criado sem senha.")
+print(f"the pdf has been un locked")
